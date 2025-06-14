19b3bd423662abc514dba532d0571c8d8c9e4b37
Camilo Betancourt Nieto <camal9620@gmail.com>
2025-06-14 13:35:51 -0500

    Update 5-preprocessing-associate-trajs-events-filtered

--- attendee_profiling/config.py
+++ attendee_profiling/config.py
@@ -9,6 +9,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DATASETS_PATH = Path(os.getenv("DATASETS_PATH"))
 INPUT_PATH = DATASETS_PATH / "Input"
 PROCESSED_PATH = DATASETS_PATH / "Processed"
+CLIPPED_POLYGONS_PATH = PROCESSED_PATH / "Zonas SONAR clipped"
 LOCATIONS_SPLIT_FILTERED_PATH = PROCESSED_PATH / "wifi_traces_filtered_split"
 PREPROCESSED_TRAJECTORIES_PATH = PROCESSED_PATH / "preprocessed_trajectories"
 TRAJECTORIES_EVENTS_PATH = PROCESSED_PATH / "trajectories_events"
